aa3ed7c1911229033abfb3f58122343a02429979
Philipp <philipp.kathmann@hs-bremen.de>
2020-01-27 18:39:06 +0100

    edit and write with scapy

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ Follow a TCP stream with pyshark.
 
 """
 import pyshark
+from scapy.all import wrpcap, rdpcap
 
 # Change FILENAME to your pcap file's name.
 from pyshark import FileCapture
@@ -25,15 +26,23 @@ while True:
         break
     try:
         # print data from the selected stream
-         if p.telnet.data == "Password:":
-            PW_Detected = 1
-         if ord(p.telnet.data[0]) == 92:
-            PW_Detected = 0
-         if PW_Detected == 1:
+        if PW_Detected == 1:
             print(p.number)
-            pwlines.append(p.number)
+            pwlines.append(int(p.number)-1)
+        if p.telnet.data == "Password:":
+            PW_Detected = 1
+        if ord(p.telnet.data[0]) == 92:
+            PW_Detected = 0
+
 
     except AttributeError:  # Skip the ACKs.Data: \r
         pass
 
 print(pwlines)
+## read file
+pkts = rdpcap(FILENAME)
+print (pkts)
+for i in pwlines:
+    pkts[i]["Raw"].load = "x"
+
+wrpcap("mod.pcap",pkts)
